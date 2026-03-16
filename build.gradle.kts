25c78b81f894af8cdde4a26681233fa50808088d
LangYa466 <langya466@gmail.com>
2026-03-16 10:41:00 +0800

    [style] ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "cn.langya"
+group = "net.linktofur"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -34,4 +34,8 @@ tasks.jar {
         }.standardOutput.asText.get().trim()
     }.getOrDefault("local")
     archiveFileName.set("LinkToFur-Server-${commitHash}.jar")
+}
+
+tasks.test {
+    enabled = false
 }
